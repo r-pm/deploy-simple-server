1ca4ccc531ef9dfea04039b91816a46473afe2da
r-pm <50608458+r-pm@users.noreply.github.com>
2021-05-12 19:24:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const app = express();
 app.use(express.json());
 
 app.use((req,res,next)=>{
-  res.send("important information from my computer")
+  res.send("welcome to this bby server")
+})
+app.get('/cute', (req,res,next)=>{
+  res.send("isn't this so cute?")
 })
 //heroku injects the port number into the PORT env value
 const port = process.env.PORT || 3001;
